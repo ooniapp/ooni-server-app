fb732512bc99e199a620f26f6b1008179c2227ac
POTCHONA Justin <potchjust@gmail.com>
2021-05-03 18:45:51 +0000

    dfs

--- db/index.js
+++ db/index.js
@@ -1,19 +1,12 @@
-const result = require("dotenv").config();
+
 
 if (result.error) {
   throw result.error;
 }
-const env = result.parsed;
 
 const knex = require("knex")({
   client: "pg",
-  connection: {
-    host: env.DB_HOST,
-    port: env.DB_PORT || 5432,
-    user: env.DB_USER,
-    password: env.DB_PASS,
-    database: env.DB_DATABASE
-  }
+  connection: DB_URL,
 });
 
 module.exports = knex;
